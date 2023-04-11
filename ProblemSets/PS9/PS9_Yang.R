1fb2c2faf83969d081a65348d282d5346b13fb02
Econ Account <ouecon016@schooner2.oscer.ou.edu>
2023-04-11 10:49:29 -0500

    Completed PS9

---
+++ ProblemSets/PS9/PS9_Yang.R
@@ -0,0 +1,120 @@
+library(tidyverse)
+library(tidymodels)
+library(magrittr)
+library(glmnet)
+
+set.seed(123456)
+
+housing <- read_table("http://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data", col_names = FALSE)
+names(housing) <- c("crim","zn","indus","chas","nox","rm","age","dis","rad","tax","ptratio","b","lstat","medv")
+
+housing_split <- initial_split(housing, prop = 0.8)
+housing_train <- training(housing_split)
+housing_test  <- testing(housing_split)
+
+housing_recipe <- recipe(medv ~ ., data = housing_train) %>%
+  # convert outcome variable to logs
+  step_log(all_outcomes()) %>%
+  # convert 0/1 chas to a factor
+  step_bin2factor(chas) %>%
+  # create interaction term between crime and nox
+  step_interact(terms = ~ crim:nox) %>%
+  # create square terms of some continuous variables
+  step_poly(dis,nox) %>%
+  prep()
+
+
+housing_train_prepped <- housing_recipe %>% juice
+housing_test_prepped  <- housing_recipe %>% bake(new_data = housing_test)
+
+
+housing_train_x <- housing_train_prepped %>% select(-medv)
+housing_test_x  <- housing_test_prepped  %>% select(-medv)
+housing_train_y <- housing_train_prepped %>% select( medv)
+housing_test_y  <- housing_test_prepped  %>% select( medv)
+
+#::::::::::::::::::::::::::::::::
+# cross-validate the lambda (LASSO)
+#::::::::::::::::::::::::::::::::
+tune_spec <- linear_reg(
+  penalty = tune(), # tuning parameter
+  mixture = 1       # 1 = lasso, 0 = ridge
+) %>% 
+  set_engine("glmnet") %>%
+  set_mode("regression")
+
+# define a grid over which to try different values of lambda
+lambda_grid <- grid_regular(penalty(), levels = 50)
+
+# 6-fold cross-validation
+rec_folds <- vfold_cv(housing_train_prepped, v = 6)
+
+# Workflow
+rec_wf <- workflow() %>%
+  add_formula(log(medv) ~ .) %>%
+  add_model(tune_spec) #%>%
+#add_recipe(housing_recipe)
+
+# Tuning results
+rec_res <- rec_wf %>%
+  tune_grid(
+    resamples = rec_folds,
+    grid = lambda_grid
+  )
+
+top_rmse  <- show_best(rec_res, metric = "rmse")
+best_rmse <- select_best(rec_res, metric = "rmse")
+
+# Now train with tuned lambda
+final_lasso <- finalize_workflow(rec_wf, best_rmse)
+
+# Print out results in test set
+last_fit(final_lasso, split = housing_split) %>%
+  collect_metrics() %>% print
+
+top_rmse %>% print(n = 1)
+
+
+#::::::::::::::::::::::::::::::::
+# cross-validate the lambda (RIDGE)
+#::::::::::::::::::::::::::::::::
+tune_spec <- linear_reg(
+  penalty = tune(), # tuning parameter
+  mixture = 0       # 1 = lasso, 0 = ridge
+) %>% 
+  set_engine("glmnet") %>%
+  set_mode("regression")
+
+# define a grid over which to try different values of lambda
+lambda_grid <- grid_regular(penalty(), levels = 50)
+
+# 6-fold cross-validation
+rec_folds <- vfold_cv(housing_train_prepped, v = 6)
+
+# Workflow
+rec_wf <- workflow() %>%
+  add_formula(log(medv) ~ .) %>%
+  add_model(tune_spec) #%>%
+#add_recipe(housing_recipe)
+
+# Tuning results
+rec_res <- rec_wf %>%
+  tune_grid(
+    resamples = rec_folds,
+    grid = lambda_grid
+  )
+
+top_rmse  <- show_best(rec_res, metric = "rmse")
+best_rmse <- select_best(rec_res, metric = "rmse")
+
+# Now train with tuned lambda
+final_lasso <- finalize_workflow(rec_wf, best_rmse)
+
+# Print out results in test set
+last_fit(final_lasso, split = housing_split) %>%
+  collect_metrics() %>% print
+
+top_rmse %>% print(n = 1)
+
+
+
